0d199df63062d6740194f58d4c4e0c580190fc89
Rahmatulah <blcak.gun123@gmail.com>
2020-05-21 01:19:40 +0700

    menambahkan style baru pada view detail mahasiswa

--- app/views/mahasiswa/details.php
+++ app/views/mahasiswa/details.php
@@ -1,18 +1,36 @@
-<div class="card" style="width: 18rem;">
+<div class="card mt-2" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">
-        <?= $data['mhs']["nama_mhs"]; ?>
+        <?= $data["judul"]; ?>
     </h5>
     <h6 class="card-subtitle mb-2 text-muted">
+        <label>
+            NPM :
+        </label>
         <?= $data['mhs']["npm"]; ?>
     </h6>
     <h6 class="card-text text-muted">
+        <label >
+            Nama Mahasiswa :
+        </label>
+        <?= $data['mhs']["nama_mhs"]; ?>
+    </h6>
+    <h6 class="card-text text-muted">
+        <label >
+            Jurusan :
+        </label>
         <?= $data['mhs']["jurusan"]; ?>
     </h6>
     <h6 class="card-text text-muted">
+        <label >
+            Semester :
+        </label>
         <?= $data['mhs']["semester"]; ?>
     </h6>
     <h6 class="card-text text-muted">
+        <label >
+            Kelas :
+        </label>
         <?= $data['mhs']["kelas"]; ?>
     </h6>
     <a href="<?= BASEURL; ?>/mahasiswa" class="card-link">&lt;&lt; Back</a>
